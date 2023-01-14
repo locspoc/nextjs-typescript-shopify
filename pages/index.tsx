cde73a6f9520717728ba550d6204554683783fae
Loc Tran <mrlocspoc@gmail.com>
2023-01-14 18:49:59 +1030

    7. Ts Config complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 export default function Home() {
 	const message: string = 'Hello World!';
+
 	let age: number = 30;
+	let a: AddEventListenerOptions;
+	let b: NodeJS.Process;
+
+	console.log('b.browser: ', b.browser);
+
+	let person: Person = {
+		name: 'Fillip',
+	};
+
 	return (
 		<div>
 			{message} {age}
