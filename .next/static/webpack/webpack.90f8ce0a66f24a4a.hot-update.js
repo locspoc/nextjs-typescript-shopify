e3183b307386c1f142d756e5f9480b0a3acaa4d3
Loc Tran <mrlocspoc@gmail.com>
2023-01-14 18:33:50 +1030

    5. Ts Init complete

--- .next/static/webpack/webpack.90f8ce0a66f24a4a.hot-update.js
+++ .next/static/webpack/webpack.90f8ce0a66f24a4a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7a8d408856afcb7a"; }
+/******/ 	__webpack_require__.h = function() { return "e6f70c838d9a6ef0"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	const message: string = 'Hello World!';
+	let age: number = 30;
+	return (
+		<div>
+			{message} {age}
+		</div>
+	);
+}
