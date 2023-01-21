1a0fa1103086b24d0da3ba811db85e835cf9a39b
Loc Tran <mrlocspoc@gmail.com>
2023-01-22 09:25:55 +1030

    9. [ai] Function params complete

--- playground.ts
+++ playground.ts
@@ -1,3 +1,12 @@
 export default function play() {
-	console.log('Hello world');
+	const name: string = 'Filip';
+	const age: number = 30;
+
+	function logPersonInfo(personName: string, personAge: number) {
+		// const info = "Name: " + personName + ", age: " + personAge;
+		const info = `Name: ${personName}, age: ${personAge}`;
+		console.log(info);
+		return info;
+	}
+	logPersonInfo(name, age);
 }
