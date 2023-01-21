92a226fe2b5bff1db7eb9df291283ca292d1d63e
Loc Tran <mrlocspoc@gmail.com>
2023-01-21 20:57:32 +1030

    Console.log not working?

---
+++ .next/static/bjPx-DQJWsqTw7imHthwj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4e8b44508a80fd19.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
