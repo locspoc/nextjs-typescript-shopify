fdf68d111e5b0395af7a9dca36af824a3e76baea
Loc Tran <mrlocspoc@gmail.com>
2023-02-04 13:56:30 +1030

    20. [ai] Union narrowing complete

--- playground.ts
+++ playground.ts
@@ -15,18 +15,36 @@ interface BusinessPerson extends Person {
 
 type RaceCar = {
 	name: string;
-	maxSpeed: number;
+	maxSpeed: 200;
+	team: string;
 };
 
 type CityCar = {
 	name: string;
 	space: string;
+	maxSpeed: 100;
 };
 
 type Car = RaceCar | CityCar;
 
 export default function play() {
+	const car: RaceCar = {
+		name: 'Race Car',
+		maxSpeed: 200,
+		team: 'ferrari',
+	};
+
 	function logCarInfo(car: Car) {
 		console.log(car.name);
+		switch (car.maxSpeed) {
+			case 200:
+				console.log(car.team);
+				break;
+			case 100:
+				console.log(car.space);
+				break;
+			default:
+				console.log(car);
+		}
 	}
 }
