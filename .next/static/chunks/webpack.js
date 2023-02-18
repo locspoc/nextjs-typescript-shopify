c0a6ba287d73ec26b51918b53293429b70f0d07c
Loc Tran <mrlocspoc@gmail.com>
2023-02-18 19:58:58 +1030

    35. [ai] Ternary with Generics complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec8780f87bbcd0b0"; }
+/******/ 		__webpack_require__.h = function() { return "35be73598e94aaab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- playground.ts
+++ playground.ts
@@ -13,21 +13,24 @@ interface PostGraduateStudent extends Person {
 	projects: string[];
 }
 
-// type StudentInfo<T extends Student = Student> = {
-// 	data: Student;
-// 	grades: number[];
-// };
+type StudentInfo<T extends any = Student> = T extends Student
+	? {
+			data: T;
+			grades: number[];
+	  }
+	: string;
 
-interface StudentInfo<T extends Student = Student> {
-	data: T;
-	grades: number[];
-}
+// interface StudentInfo<T extends Student = Student> {
+// 	data: T;
+// 	grades: number[];
+// }
+
+type Car = { engine: string };
 
 export default function play() {
-	function logStudentInfo(info: StudentInfo<PostGraduateStudent>) {
-		console.log(info.data.name);
-		console.log(info.data.age);
-		console.log(info.data.projects);
+	function logStudentInfo(info: StudentInfo<Car>) {
+		console.log(info);
+		console.log(info);
 	}
 
 	const info = {
