5b3528964848ae051656c78a5ee73611750b1dd4
Loc Tran <mrlocspoc@gmail.com>
2023-03-10 19:52:09 +1030

    40. Typeof complete

--- playground.ts
+++ playground.ts
@@ -2,21 +2,23 @@ interface Person {
 	name: string;
 }
 
-type CustomArray<T> = {
-	[index: number]: T;
-};
-
-type CustomObject<T = string | number | Person> = {
-	[key: string]: T;
-};
-
 export default function play() {
-	const person: CustomObject<string | number | Person> = {
-		age: '23',
-		name: 'asdasd',
-		city: 'sadsad',
-		person: {
-			name: 'Filip',
-		},
+	function logger(...args: any[]) {
+		return 'hello world';
+	}
+
+	const kindaLogger: typeof logger = (name: string, age: number) =>
+		'Hi Guys!';
+
+	const person = {
+		name: 'Filip',
+		age: 30,
 	};
+
+	const person2: typeof person = {
+		name: 'John',
+		age: 35,
+	};
+
+	kindaLogger('', 20);
 }
