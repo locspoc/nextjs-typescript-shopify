3632713435d936c4fd5a9e8719ebe6040752d86c
Loc Tran <mrlocspoc@gmail.com>
2023-03-10 19:40:33 +1030

    39. Mapped Types complete

--- playground.ts
+++ playground.ts
@@ -6,7 +6,17 @@ type CustomArray<T> = {
 	[index: number]: T;
 };
 
+type CustomObject<T = string | number | Person> = {
+	[key: string]: T;
+};
+
 export default function play() {
-	const items: CustomArray<string> = ['1', '2', '3'];
-	const items2: CustomArray<number> = [1, 2, 3];
+	const person: CustomObject<string | number | Person> = {
+		age: '23',
+		name: 'asdasd',
+		city: 'sadsad',
+		person: {
+			name: 'Filip',
+		},
+	};
 }
