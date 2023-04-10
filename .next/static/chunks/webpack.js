19da2be53e00234493ab41a777a667d1e00b8678
Loc Tran <mrlocspoc@gmail.com>
2023-04-10 09:31:42 +0930

    46. Infer get static props complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b99cebc461a5ff0f"; }
+/******/ 		__webpack_require__.h = function() { return "695edb0e5562b18a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import { useEffect } from 'react';
-import play from '../playground';
+import type {
+	InferGetServerSidePropsType,
+	InferGetStaticPropsType,
+} from 'next';
 
-export default function Home() {
-	useEffect(() => {
-		play();
-	}, []);
+export async function getStaticProps() {
+	const products = [1, 2, 3];
+	return {
+		props: {
+			products,
+		},
+		revalidate: 4 * 60 * 60,
+	};
+}
 
-	return <div>Hello world</div>;
+export default function Home({
+	products,
+}: InferGetServerSidePropsType<typeof getStaticProps>) {
+	return <div>{products}</div>;
 }
