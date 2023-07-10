8307143611f970a62b585b52a9e182b3c66ae8a9
Loc Tran <mrlocspoc@gmail.com>
2023-07-10 23:05:37 +0930

    60. Alias Imports complete

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6ee64c75883470e5"; }
+/******/ 		__webpack_require__.h = function() { return "97652a89868e9378"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -2,7 +2,7 @@ import { ImageEdge, Product as ShopifyProduct } from '../schema';
 
 // regexr.com/
 
-import Product from '../../common/types/product';
+import { Product } from '@common/types/product';
 
 const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) =>
 	// debugger;
